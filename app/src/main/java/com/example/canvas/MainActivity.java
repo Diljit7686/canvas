55290ba1f1f028ba1b21d33f166a58a527553218
diljit7686 <diljit7686@gmail.com>
2019-09-23 11:27:03 -0400

    Code With some drawLines

--- app/src/main/java/com/example/canvas/MainActivity.java
+++ app/src/main/java/com/example/canvas/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.canvas;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        // setup background color
+       canvas.drawColor(Color.RED);
 
 
+       // paintbrush color
+        paintbrush.setColor(Color.YELLOW);
+
+        // draw a line
+        canvas.drawLine(10,50,200,50, paintbrush);
+
+        paintbrush.setColor(Color.GREEN);
+        canvas.drawLine(10,50,200,250, paintbrush);
 
 
         // put the canvas into frame
